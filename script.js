b5105dda693e3dbe00df114d28e145eabe6719b1
THULASIKUMAR S <139734182+thulasikumar4529@users.noreply.github.com>
2024-03-30 20:38:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function openEmail() {
+  window.location.href = "mailto:kumar.thulasi4529@gmail.com";
+}
